96ff586d2954c4254cbbfe381d8b6b314508be3a
Robert Konrad <robert@ktx-software.com>
2015-09-08 16:21:29 -0700

    Updated script for new binary names.

--- copysysbin.sh
+++ copysysbin.sh
@@ -1,6 +1,6 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	cp "$( dirname "${BASH_SOURCE[0]}" )"/iojs-linux64 "$( dirname "${BASH_SOURCE[0]}" )"/iojs
+	cp "$( dirname "${BASH_SOURCE[0]}" )"/node-linux64 "$( dirname "${BASH_SOURCE[0]}" )"/node
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	cp "$( dirname "${BASH_SOURCE[0]}" )"/iojs-osx "$( dirname "${BASH_SOURCE[0]}" )"/iojs
+	cp "$( dirname "${BASH_SOURCE[0]}" )"/node-osx "$( dirname "${BASH_SOURCE[0]}" )"/node
 fi
 
