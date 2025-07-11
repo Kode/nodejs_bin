42de921a867d88bfc0731b434a939b8689d47a45
Robert Konrad <git2016@robdangero.us>
2025-07-11 20:56:30 +0200

    Use arch-specific Mac binaries

--- copysysbin.sh
+++ copysysbin.sh
@@ -1,13 +1,23 @@
 if [[ $OSTYPE == linux-gnu* ]]; then
 	arch=$(uname -m)
 	if [[ "$arch" == "aarch64" ]]; then
-		cp "$( dirname "${BASH_SOURCE[0]}" )"/node-linuxaarch64 "$( dirname "${BASH_SOURCE[0]}" )"/node
+		cp "$( dirname "${BASH_SOURCE[0]}" )"/node-linux_arm64 "$( dirname "${BASH_SOURCE[0]}" )"/node
 	elif [[ "$arch" == "x86_64" ]]; then
-		cp "$( dirname "${BASH_SOURCE[0]}" )"/node-linux64 "$( dirname "${BASH_SOURCE[0]}" )"/node
-	else  # Assume 32-bit ARM
-		cp "$( dirname "${BASH_SOURCE[0]}" )"/node-linuxarm "$( dirname "${BASH_SOURCE[0]}" )"/node
+		cp "$( dirname "${BASH_SOURCE[0]}" )"/node-linux_x64 "$( dirname "${BASH_SOURCE[0]}" )"/node
+	elif [[ "$arch" == "armv" ]]; then
+		cp "$( dirname "${BASH_SOURCE[0]}" )"/node-linux_arm "$( dirname "${BASH_SOURCE[0]}" )"/node
+	else
+		echo "Unknown Linux machine '$MACHINE_TYPE', please edit copysysbin.sh"
+		exit 1
 	fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	cp "$( dirname "${BASH_SOURCE[0]}" )"/node-osx "$( dirname "${BASH_SOURCE[0]}" )"/node
+	arch=$(uname -m)
+	if [[ "$arch" == "arm64" ]]; then
+		cp "$( dirname "${BASH_SOURCE[0]}" )"/node-macos_arm64 "$( dirname "${BASH_SOURCE[0]}" )"/node
+	elif [[ "$arch" == "x86_64" ]]; then
+		cp "$( dirname "${BASH_SOURCE[0]}" )"/node-macos_x64 "$( dirname "${BASH_SOURCE[0]}" )"/node
+	else
+		echo "Unknown macOS machine '$MACHINE_TYPE', please edit copysysbin.sh"
+		exit 1
+	fi
 fi
-
